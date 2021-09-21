db2eba6c5ca3aad5bfd81f227be07bf85ea8e391
JanCaha <jan.caha@outlook.com>
2021-09-21 13:04:55 +0200

    read and write layout item

--- BivariateRenderer/layoutitems/layout_item.py
+++ BivariateRenderer/layoutitems/layout_item.py
@@ -1,4 +1,4 @@
-from typing import NoReturn, List
+from typing import NoReturn, List, Optional
 
 from qgis.PyQt.QtGui import QColor
 from qgis.PyQt.QtXml import QDomDocument, QDomElement
@@ -10,7 +10,11 @@ from qgis.core import (QgsLayoutItem,
                        QgsTextFormat,
                        QgsLayoutItemRenderContext,
                        QgsLineSymbol,
-                       QgsReadWriteContext)
+                       QgsReadWriteContext,
+                       QgsSymbolLayerUtils,
+                       QgsSymbol,
+                       QgsProject,
+                       QgsMapLayerType)
 
 from ..text_constants import Texts, IDS
 from ..utils import log, get_symbol_object, get_symbol_dict
@@ -26,6 +30,12 @@ class BivariateRendererLayoutItem(QgsLayoutItem):
 
     layer: QgsVectorLayer = None
     renderer: BivariateRenderer
+
+    text_axis_x: str
+    text_axis_y: str
+
+    text_format: QgsTextFormat
+    line_format: QgsSymbol
 
     def __init__(self, layout: QgsLayout):
 
@@ -62,7 +72,31 @@ class BivariateRendererLayoutItem(QgsLayoutItem):
                                  item_size.height(),
                                  self.renderer.generate_legend_polygons())
 
-    def writePropertiesToElement(self, element: QDomElement, document: QDomDocument, context: QgsReadWriteContext) -> bool:
+    def writePropertiesToElement(self,
+                                 bivariate_legend_element: QDomElement,
+                                 doc: QDomDocument,
+                                 context: QgsReadWriteContext) -> bool:
+
+        bivariate_legend_element.setAttribute("axis_x_name", self.text_axis_x)
+        bivariate_legend_element.setAttribute("axis_y_name", self.text_axis_y)
+
+        line_symbol = doc.createElement("lineSymbol")
+
+        symbol_elem = QgsSymbolLayerUtils.saveSymbol("",
+                                                     self.line_format,
+                                                     doc,
+                                                     context)
+
+        line_symbol.appendChild(symbol_elem)
+
+        bivariate_legend_element.appendChild(line_symbol)
+
+        text_elem = self.text_format.writeXml(doc, context)
+
+        symbol_elem.appendChild(text_elem)
+
+        if self.layer:
+            bivariate_legend_element.setAttribute("vectorLayerId", self.layer.id())
 
         # line
         # https://github.com/qgis/QGIS/blob/32c2cea54cb92bbb2243b222816c8154c2b9adf9/src/core/layout/qgslayoutitemscalebar.cpp#L831
@@ -70,20 +104,59 @@ class BivariateRendererLayoutItem(QgsLayoutItem):
         # text
         # https://github.com/qgis/QGIS/blob/32c2cea54cb92bbb2243b222816c8154c2b9adf9/src/core/layout/qgslayoutitemscalebar.cpp#L765
 
-        # todo add
-        pass
+        # layer
+        # https://github.com/qgis/QGIS/blob/32c2cea54cb92bbb2243b222816c8154c2b9adf9/src/core/layout/qgslayoutitemattributetable.cpp#L840
 
-    # def readPropertiesFromElement(self, element: QtXml.QDomElement, document: QtXml.QDomDocument, context: QgsReadWriteContext) -> bool:
-    #     # todo add
+        return True
+
+    def readPropertiesFromElement(self,
+                                  element: QDomElement,
+                                  document: QDomDocument,
+                                  context: QgsReadWriteContext) -> bool:
+
+        if self.linked_layer:
+            self.layer = None
+
+        self.axis_x_name = element.attribute("axis_x_name")
+        self.axis_y_name = element.attribute("axis_y_name")
+
+        if element.hasAttribute("vectorLayerId"):
+
+            layerId = element.attribute("vectorLayerId")
+
+            layer = QgsProject.instance().mapLayer(layerId)
+
+            if layer:
+
+                if layer.type() == QgsMapLayerType.VectorLayer:
+
+                    layer: QgsVectorLayer
+
+                    if layer.renderer().type() == Texts.bivariate_renderer_short_name:
+                        self.layer = layer
+                        self.renderer = layer.renderer()
+
+        lineSymbolElem = element.firstChildElement("lineSymbol")
+
+        if not lineSymbolElem.isNull():
+
+            symbolElem = lineSymbolElem.firstChildElement("symbol")
+            self.line_format = QgsSymbolLayerUtils.loadSymbol(symbolElem, context)
+
+        textFormatNodeList = element.elementsByTagName("text-style")
+
+        if not textFormatNodeList.isEmpty():
+
+            textFormatElem = textFormatNodeList.at(0).toElement()
+            self.text_format.readXml(textFormatElem, context)
+
+        return True
 
         # line
         # https://github.com/qgis/QGIS/blob/32c2cea54cb92bbb2243b222816c8154c2b9adf9/src/core/layout/qgslayoutitemscalebar.cpp#L894
 
         # text
         # https://github.com/qgis/QGIS/blob/32c2cea54cb92bbb2243b222816c8154c2b9adf9/src/core/layout/qgslayoutitemscalebar.cpp#L979
-
-
-    #     pass
 
     def set_linked_layer(self, layer: QgsVectorLayer) -> NoReturn:
         self.layer = layer
@@ -109,6 +182,14 @@ class BivariateRendererLayoutItem(QgsLayoutItem):
     @property
     def get_font(self):
         return self.text_axis_x.font()
+
+    @property
+    def linked_layer(self) -> Optional[QgsVectorLayer]:
+
+        if self.layer is not None:
+            return self.layer
+
+        return None
 
     @property
     def linked_layer_name(self):
