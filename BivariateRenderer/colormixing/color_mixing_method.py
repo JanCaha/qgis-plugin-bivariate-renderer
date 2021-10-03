9b05d515f6daa80e51b9d3f957b82cb27c49f408
Jan Caha <jan.caha@outlook.com>
2021-10-03 16:43:41 +0200

    make code more readable

--- BivariateRenderer/colormixing/color_mixing_method.py
+++ BivariateRenderer/colormixing/color_mixing_method.py
@@ -23,6 +23,7 @@ class ColorMixingMethodDirect(ColorMixingMethod):
         return "Direct color mixing"
 
     def mix_colors(self, color1: QColor, color2: QColor) -> QColor:
+
         return QColor(int((color1.red() + color2.red())/2),
                       int((color1.green() + color2.green())/2),
                       int((color1.blue() + color2.blue())/2))
@@ -37,6 +38,7 @@ class ColorMixingMethodDarken(ColorMixingMethod):
         return "Darken blend color mixing"
 
     def mix_colors(self, color1: QColor, color2: QColor) -> QColor:
+
         return QColor(min(color1.red(), color2.red()),
                       min(color1.green(), color2.green()),
                       min(color1.blue(), color2.blue()))
