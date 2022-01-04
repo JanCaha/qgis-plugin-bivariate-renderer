1dc588a0174e1f1ea6ee72fd4e4f139780905540
Jan Caha <jan.caha@outlook.com>
2022-01-04 21:40:36 +0000

    add fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
+from pathlib import Path
 
-from qgis.core import QgsProject, QgsLayout
+from qgis.core import QgsProject, QgsLayout, QgsVectorLayer
 
 
 @pytest.fixture
@@ -11,3 +12,11 @@ def qgs_project() -> QgsProject:
 @pytest.fixture
 def qgs_layout(qgs_project) -> QgsLayout:
     return QgsLayout(qgs_project)
+
+
+@pytest.fixture
+def nc_layer() -> QgsVectorLayer:
+
+    path = Path(__file__).parent / "data" / "nc_data.gpkg"
+
+    return QgsVectorLayer(f"{path.as_posix()}|layername=nc_data", "layer", "ogr")
