d932f9364a48a72cc3cd3d31eb702fafac8ec2f4
Jan Caha <jan.caha@outlook.com>
2022-01-06 09:35:24 +0000

    generate new images for tests

--- tests/test_generate_correct_images.py
+++ tests/test_generate_correct_images.py
@@ -4,6 +4,7 @@ from qgis.PyQt.QtGui import QPainter, QImage, qRgba
 from qgis.core import (QgsLayoutUtils)
 
 from BivariateRenderer.legendrenderer.legend_renderer import LegendRenderer
+from BivariateRenderer.colormixing.color_mixing_method import ColorMixingMethodDarken, ColorMixingMethodDirect
 
 from tests import set_up_bivariate_renderer
 
@@ -124,3 +125,63 @@ def test_generate_legend_with_arrows_text_rotated(qgis_countries_layer, qgs_proj
     painter.end()
 
     image.save("./tests/images/correct/legend_with_arrows_texts_rotated.png", "PNG")
+
+
+@skip_setting
+def test_generate_legend_darken(qgis_countries_layer, qgs_project, qgs_layout):
+
+    legend_size = 500
+
+    image = QImage(legend_size, legend_size, QImage.Format_ARGB32)
+    image.fill(qRgba(0, 0, 0, 0))
+    assert isinstance(image, QImage)
+
+    painter = QPainter(image)
+
+    render_context = QgsLayoutUtils.createRenderContextForLayout(qgs_layout, painter)
+
+    assert render_context
+
+    bivariate_renderer = set_up_bivariate_renderer(qgis_countries_layer,
+                                                   field1="fid",
+                                                   field2="fid")
+    bivariate_renderer.color_mixing_method = ColorMixingMethodDarken()
+
+    legend_renderer = LegendRenderer()
+    legend_renderer.render(render_context, legend_size / render_context.scaleFactor(),
+                           legend_size / render_context.scaleFactor(),
+                           bivariate_renderer.generate_legend_polygons())
+
+    painter.end()
+
+    image.save("./tests/images/correct/legend_only_darken.png", "PNG")
+
+
+@skip_setting
+def test_generate_legend_direct_mixing(qgis_countries_layer, qgs_project, qgs_layout):
+
+    legend_size = 500
+
+    image = QImage(legend_size, legend_size, QImage.Format_ARGB32)
+    image.fill(qRgba(0, 0, 0, 0))
+    assert isinstance(image, QImage)
+
+    painter = QPainter(image)
+
+    render_context = QgsLayoutUtils.createRenderContextForLayout(qgs_layout, painter)
+
+    assert render_context
+
+    bivariate_renderer = set_up_bivariate_renderer(qgis_countries_layer,
+                                                   field1="fid",
+                                                   field2="fid")
+    bivariate_renderer.color_mixing_method = ColorMixingMethodDirect()
+
+    legend_renderer = LegendRenderer()
+    legend_renderer.render(render_context, legend_size / render_context.scaleFactor(),
+                           legend_size / render_context.scaleFactor(),
+                           bivariate_renderer.generate_legend_polygons())
+
+    painter.end()
+
+    image.save("./tests/images/correct/legend_only_direct_mixing.png", "PNG")
