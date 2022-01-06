88aad517c846c577bb953b54b330173b00e06b7b
Jan Caha <jan.caha@outlook.com>
2022-01-06 14:03:31 +0000

    add test_generate_legend_in_layout()

--- tests/test_generate_correct_images.py
+++ tests/test_generate_correct_images.py
@@ -5,8 +5,10 @@ from qgis.core import (QgsLayoutUtils)
 
 from BivariateRenderer.legendrenderer.legend_renderer import LegendRenderer
 from BivariateRenderer.colormixing.color_mixing_method import ColorMixingMethodDarken, ColorMixingMethodDirect
+from BivariateRenderer.colorramps.bivariate_color_ramp import BivariateColorRampGreenPink
+from BivariateRenderer.layoutitems.layout_item import BivariateRendererLayoutItem
 
-from tests import set_up_bivariate_renderer
+from tests import set_up_bivariate_renderer, set_up_layout_page_a4, get_layout_space, export_page_to_image
 
 skip_setting = pytest.mark.skipif(True, reason="do not generate with every run")
 
@@ -185,3 +187,27 @@ def test_generate_legend_direct_mixing(qgis_countries_layer, qgs_project, qgs_la
     painter.end()
 
     image.save("./tests/images/correct/legend_only_direct_mixing.png", "PNG")
+
+
+@skip_setting
+def test_generate_legend_in_layout(qgis_countries_layer, qgs_layout, qgs_project):
+
+    page = set_up_layout_page_a4(qgs_layout)
+
+    bivariate_renderer = set_up_bivariate_renderer(qgis_countries_layer,
+                                                   field1="fid",
+                                                   field2="fid",
+                                                   color_ramps=BivariateColorRampGreenPink())
+
+    qgis_countries_layer.setRenderer(bivariate_renderer)
+
+    qgs_project.addMapLayer(qgis_countries_layer)
+
+    layout_item = BivariateRendererLayoutItem(qgs_layout)
+    layout_item.set_linked_layer(qgis_countries_layer)
+
+    layout_item.attemptSetSceneRect(get_layout_space())
+
+    qgs_layout.addItem(layout_item)
+
+    export_page_to_image(qgs_layout, page, "./tests/images/correct/layout_item_legend.png")
