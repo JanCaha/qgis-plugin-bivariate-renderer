568d522a6d48de6e46921885109f13d4d86d969d
Jan Caha <jan.caha@outlook.com>
2022-04-24 22:28:46 +0200

    add creation

--- tests/test_generate_correct_images.py
+++ tests/test_generate_correct_images.py
@@ -211,3 +211,117 @@ def test_generate_legend_in_layout(qgis_countries_layer, qgs_layout, qgs_project
     qgs_layout.addItem(layout_item)
 
     export_page_to_image(qgs_layout, page, "./tests/images/correct/layout_item_legend.png")
+
+
+@skip_setting
+def test_legend_ticks(qgis_countries_layer, qgs_project, qgs_layout):
+
+    legend_size = 500
+
+    image = QImage(legend_size, legend_size, QImage.Format_ARGB32)
+    image.fill(qRgba(0, 0, 0, 0))
+    assert isinstance(image, QImage)
+
+    painter = QPainter(image)
+
+    render_context = QgsLayoutUtils.createRenderContextForLayout(qgs_layout, painter)
+
+    assert render_context
+
+    bivariate_renderer = set_up_bivariate_renderer(qgis_countries_layer,
+                                                   field1="fid",
+                                                   field2="fid")
+    bivariate_renderer.color_mixing_method = ColorMixingMethodDirect()
+
+    legend_renderer = LegendRenderer()
+    legend_renderer.add_axes_arrows = False
+    legend_renderer.add_axes_texts = False
+    legend_renderer.legend_rotated = False
+    legend_renderer.add_axes_ticks_texts = True
+    legend_renderer.texts_axis_x_ticks = bivariate_renderer.field_1_labels
+    legend_renderer.texts_axis_y_ticks = bivariate_renderer.field_2_labels
+
+    legend_renderer.render(render_context,
+                           image.width() / render_context.scaleFactor(),
+                           image.width() / render_context.scaleFactor(),
+                           bivariate_renderer.generate_legend_polygons())
+
+    painter.end()
+
+    image.save("./tests/images/correct/legend_with_values_ticks.png", "PNG")
+
+
+@skip_setting
+def test_legend_all(qgis_countries_layer, qgs_project, qgs_layout):
+
+    legend_size = 500
+
+    image = QImage(legend_size, legend_size, QImage.Format_ARGB32)
+    image.fill(qRgba(0, 0, 0, 0))
+    assert isinstance(image, QImage)
+
+    painter = QPainter(image)
+
+    render_context = QgsLayoutUtils.createRenderContextForLayout(qgs_layout, painter)
+
+    assert render_context
+
+    bivariate_renderer = set_up_bivariate_renderer(qgis_countries_layer,
+                                                   field1="fid",
+                                                   field2="fid")
+
+    legend_renderer = LegendRenderer()
+    legend_renderer.add_axes_arrows = True
+    legend_renderer.add_axes_texts = True
+    legend_renderer.legend_rotated = False
+    legend_renderer.add_axes_ticks_texts = True
+    legend_renderer.texts_axis_x_ticks = bivariate_renderer.field_1_labels
+    legend_renderer.texts_axis_y_ticks = bivariate_renderer.field_2_labels
+
+    legend_renderer.render(render_context,
+                           image.width() / render_context.scaleFactor(),
+                           image.width() / render_context.scaleFactor(),
+                           bivariate_renderer.generate_legend_polygons())
+
+    painter.end()
+
+    image.save("./tests/images/correct/legend_with_all.png", "PNG")
+
+
+@skip_setting
+def test_legend_all_rotated(qgis_countries_layer, qgs_project, qgs_layout):
+
+    legend_size = 500
+
+    image = QImage(legend_size, legend_size, QImage.Format_ARGB32)
+    image.fill(qRgba(0, 0, 0, 0))
+    assert isinstance(image, QImage)
+
+    painter = QPainter(image)
+
+    render_context = QgsLayoutUtils.createRenderContextForLayout(qgs_layout, painter)
+
+    assert render_context
+
+    bivariate_renderer = set_up_bivariate_renderer(qgis_countries_layer,
+                                                   field1="fid",
+                                                   field2="fid")
+
+    bivariate_renderer.color_mixing_method = ColorMixingMethodDirect()
+
+    legend_renderer = LegendRenderer()
+    legend_renderer.add_axes_arrows = True
+    legend_renderer.add_axes_texts = True
+    legend_renderer.legend_rotated = True
+    legend_renderer.add_axes_ticks_texts = True
+    legend_renderer.texts_axis_x_ticks = bivariate_renderer.field_1_labels
+    legend_renderer.texts_axis_y_ticks = bivariate_renderer.field_2_labels
+
+    legend_renderer.render(render_context,
+                           image.width() / render_context.scaleFactor(),
+                           image.width() / render_context.scaleFactor(),
+                           bivariate_renderer.generate_legend_polygons())
+
+    painter.end()
+
+    image.save("./tests/images/correct/legend_with_all_rotated.png", "PNG")
