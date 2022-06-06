f83880e12d7999645dde71fc032d072070e26b89
Jan Caha <jan.caha@outlook.com>
2022-06-06 21:22:06 +0200

    update test

--- tests/test_color_ramps.py
+++ tests/test_color_ramps.py
@@ -4,18 +4,27 @@ from qgis.core import QgsGradientColorRamp
 from qgis.PyQt.QtGui import QIcon
 
 from BivariateRenderer.colorramps.bivariate_color_ramp import (
-    BivariateColorRamp, BivariateColorRampAquamarinePink, BivariateColorRampBlueGreen,
-    BivariateColorRampDarkRedLightBlue, BivariateColorRampGreenPink, BivariateColorRampYellowPink,
-    BivariateColorRampOrangeBlue)
+    BivariateColorRamp, BivariateColorRampBlueGreen, BivariateColorRampCyanBrow,
+    BivariateColorRampCyanViolet, BivariateColorRampGreenPink, BivariateColorRampGreenPurple,
+    BivariateColorRampLigthYellowPurple, BivariateColorRampOrangeBlue,
+    BivariateColorRampOrangePurple, BivariateColorRampPinkBlue, BivariateColorRampTurquoiseGold,
+    BivariateColorRampVioletBlue, BivariateColorRampYellowBlue)
 from BivariateRenderer.colorramps.color_ramps_register import BivariateColorRampsRegister
 
 color_ramps = [
-    BivariateColorRampAquamarinePink(),
     BivariateColorRampBlueGreen(),
-    BivariateColorRampDarkRedLightBlue(),
+    BivariateColorRampCyanBrow(),
+    BivariateColorRampCyanViolet(),
+    BivariateColorRampBlueGreen(),
     BivariateColorRampGreenPink(),
-    BivariateColorRampYellowPink(),
-    BivariateColorRampOrangeBlue()
+    BivariateColorRampOrangeBlue(),
+    BivariateColorRampGreenPurple(),
+    BivariateColorRampLigthYellowPurple(),
+    BivariateColorRampOrangePurple(),
+    BivariateColorRampPinkBlue(),
+    BivariateColorRampTurquoiseGold(),
+    BivariateColorRampVioletBlue(),
+    BivariateColorRampYellowBlue()
 ]
 
 
@@ -45,5 +54,4 @@ def test_color_ramp_register():
     assert isinstance(register.icons, list)
     assert isinstance(register.icons[0], QIcon)
 
-    assert isinstance(register.get_by_name("Dark red - Light Blue"),
-                      BivariateColorRampDarkRedLightBlue)
+    assert isinstance(register.get_by_name("Orange - Purple"), BivariateColorRampOrangePurple)
