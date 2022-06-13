668f485800f12f9bead2d5ee601fe2e369ecfee1
Jan Caha <jan.caha@outlook.com>
2022-06-13 16:39:38 +0200

    add visual tests for widgets

--- tests/test_layout_item_widget.py
+++ tests/test_layout_item_widget.py
@@ -7,6 +7,8 @@ from BivariateRenderer.layoutitems.layout_item_widget import BivariateRendererLa
 from BivariateRenderer.colorramps.bivariate_color_ramp import BivariateColorRampGreenPink
 from BivariateRenderer.utils import default_line_symbol
 
+from tests import (assert_images_equal)
+
 
 def test_layout_item_widget(qgis_countries_layer: QgsVectorLayer, qgs_layout, qgis_parent,
                             qgs_project, prepare_bivariate_renderer):
@@ -48,3 +50,31 @@ def test_layout_item_widget(qgis_countries_layer: QgsVectorLayer, qgs_layout, qg
     assert isinstance(widget.b_line_symbol, QgsSymbolButton)
 
     assert isinstance(widget.layout_item, BivariateRendererLayoutItem)
+
+
+def test_visual(qgis_countries_layer: QgsVectorLayer, qgs_layout, qgis_parent, qgs_project,
+                prepare_bivariate_renderer):
+
+    qgs_project.addMapLayer(qgis_countries_layer)
+
+    bivariate_renderer = prepare_bivariate_renderer(qgis_countries_layer,
+                                                    field1="fid",
+                                                    field2="fid",
+                                                    color_ramps=BivariateColorRampGreenPink())
+
+    qgis_countries_layer.setRenderer(bivariate_renderer)
+
+    layout_item = BivariateRendererLayoutItem(qgs_layout)
+    layout_item.set_linked_layer(qgis_countries_layer)
+
+    layout_item.set_line_format(default_line_symbol())
+
+    widget = BivariateRendererLayoutItemWidget(None, layout_item)
+
+    widget.show()
+
+    pixmap = widget.grab()
+
+    pixmap.save("tests/images/image.png", "png")
+
+    assert_images_equal("tests/images/correct/widget_layout_item.png", "tests/images/image.png")
