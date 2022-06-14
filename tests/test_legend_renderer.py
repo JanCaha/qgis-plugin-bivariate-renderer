e3cb1f14dddf5fd758232a25eb9237216aee5afd
Jan Caha <jan.caha@outlook.com>
2022-06-14 22:07:22 +0200

    new test cases

--- tests/test_legend_renderer.py
+++ tests/test_legend_renderer.py
@@ -2,6 +2,7 @@ from qgis.core import (QgsTextFormat, QgsLayoutUtils)
 from qgis.PyQt.QtGui import QColor
 
 from BivariateRenderer.legendrenderer.legend_renderer import LegendRenderer
+from BivariateRenderer.renderer.bivariate_renderer import BivariateRenderer
 from BivariateRenderer.colormixing.color_mixing_method import ColorMixingMethodDirect
 from BivariateRenderer.utils import get_symbol_dict
 
@@ -345,3 +346,80 @@ def test_legend_with_spacer(qgis_countries_layer, qgs_project, qgs_layout, prepa
     image.save("./tests/images/image.png", "PNG")
 
     assert_images_equal("tests/images/correct/legend_with_spacer.png", "tests/images/image.png")
+
+
+def test_legend_with_arrows_common_origin(qgis_countries_layer, qgs_project, qgs_layout,
+                                          prepare_default_QImage, prepare_bivariate_renderer,
+                                          prepare_painter):
+
+    legend_size = 500
+
+    image = prepare_default_QImage(legend_size)
+
+    painter = prepare_painter(image)
+
+    render_context = QgsLayoutUtils.createRenderContextForLayout(qgs_layout, painter)
+
+    assert render_context
+
+    bivariate_renderer = prepare_bivariate_renderer(qgis_countries_layer,
+                                                    field1="fid",
+                                                    field2="fid")
+
+    legend_renderer = LegendRenderer()
+    legend_renderer.add_axes_arrows = True
+    legend_renderer.arrows_common_start_point = True
+    legend_renderer.render(render_context, legend_size / render_context.scaleFactor(),
+                           legend_size / render_context.scaleFactor(),
+                           bivariate_renderer.generate_legend_polygons())
+
+    painter.end()
+
+    image.save("./tests/images/image.png", "PNG")
+
+    assert_images_equal("tests/images/correct/legend_with_arrows_common_origin.png",
+                        "tests/images/image.png")
+
+
+def test_legend_ticks_midpoints(qgis_countries_layer, qgs_project, qgs_layout,
+                                prepare_default_QImage, prepare_bivariate_renderer,
+                                prepare_painter):
+
+    legend_size = 500
+
+    image = prepare_default_QImage(legend_size)
+
+    painter = prepare_painter(image)
+
+    render_context = QgsLayoutUtils.createRenderContextForLayout(qgs_layout, painter)
+
+    assert render_context
+
+    bivariate_renderer = prepare_bivariate_renderer(qgis_countries_layer,
+                                                    field1="fid",
+                                                    field2="fid")
+
+    bivariate_renderer.color_mixing_method = ColorMixingMethodDirect()
+
+    legend_renderer = LegendRenderer()
+    legend_renderer.add_axes_arrows = False
+    legend_renderer.add_axes_texts = False
+    legend_renderer.legend_rotated = False
+    legend_renderer.add_axes_ticks_texts = True
+    legend_renderer.use_category_midpoints = True
+    legend_renderer.texts_axis_x_ticks = BivariateRenderer.classes_to_legend_midpoints(
+        bivariate_renderer.field_1_classes)
+    legend_renderer.texts_axis_y_ticks = BivariateRenderer.classes_to_legend_midpoints(
+        bivariate_renderer.field_2_classes)
+
+    legend_renderer.render(render_context,
+                           image.width() / render_context.scaleFactor(),
+                           image.width() / render_context.scaleFactor(),
+                           bivariate_renderer.generate_legend_polygons())
+
+    painter.end()
+
+    image.save("./tests/images/image.png", "PNG")
+
+    assert_images_equal("tests/images/correct/legend_with_values_ticks_midpoints.png",
+                        "tests/images/image.png")
