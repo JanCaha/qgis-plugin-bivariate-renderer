996af6b4a135dea292b4cb8baa92ab37cd61bd66
Jan Caha <jan.caha@outlook.com>
2022-06-21 10:16:53 +0200

    add test case

--- tests/test_legend_renderer.py
+++ tests/test_legend_renderer.py
@@ -1,4 +1,4 @@
-from qgis.core import (QgsTextFormat, QgsLayoutUtils)
+from qgis.core import (QgsTextFormat, QgsLayoutUtils, QgsRenderContext)
 from qgis.PyQt.QtGui import QColor
 
 from BivariateRenderer.legendrenderer.legend_renderer import LegendRenderer
@@ -422,4 +422,43 @@ def test_legend_ticks_midpoints(qgis_countries_layer, qgs_project, qgs_layout,
     image.save("./tests/images/image.png", "PNG")
 
     assert_images_equal("tests/images/correct/legend_with_values_ticks_midpoints.png",
+                        "tests/images/image.png")
+
+
+def test_legend_empty_squares(qgis_countries_layer, qgs_project, qgs_layout,
+                              prepare_default_QImage, prepare_painter, prepare_bivariate_renderer):
+
+    legend_size = 500
+
+    image = prepare_default_QImage(legend_size)
+
+    painter = prepare_painter(image)
+
+    render_context = QgsLayoutUtils.createRenderContextForLayout(qgs_layout, painter)
+
+    assert render_context
+
+    bivariate_renderer = prepare_bivariate_renderer(qgis_countries_layer,
+                                                    field1="fid",
+                                                    field2="fid")
+
+    legend_renderer = LegendRenderer()
+
+    legend_renderer.add_colors_separators = True
+    legend_renderer.color_separator_width_percent = 5
+    legend_renderer.replace_rectangle_without_values = True
+    legend_renderer.use_rectangle_without_values_color_from_legend = False
+    legend_renderer.symbol_rectangle_without_values.setColor(QColor("#ffffff"))
+
+    for feature in qgis_countries_layer.getFeatures():
+        bivariate_renderer.symbolForFeature(feature, QgsRenderContext())
+
+    legend_renderer.render_legend(render_context, legend_size / render_context.scaleFactor(),
+                                  legend_size / render_context.scaleFactor(), bivariate_renderer)
+
+    painter.end()
+
+    image.save("./tests/images/image.png", "PNG")
+
+    assert_images_equal("tests/images/correct/legend_replaced_missing_values.png",
                         "tests/images/image.png")
