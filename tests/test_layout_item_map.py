c67f6bb90f40433ae34ce20a24889de5b90ee585
Jan Caha <jan.caha@outlook.com>
2026-03-02 21:48:09 +0100

    add test case

---
+++ tests/test_layout_item_map.py
@@ -0,0 +1,37 @@
+from qgis.core import QgsLayout, QgsLayoutItemMap, QgsLayoutItemPage, QgsProject, QgsVectorLayer
+
+from BivariateRenderer.colorramps.bivariate_color_ramp import BivariateColorRampGreenPink
+from tests import assert_images_equal
+
+
+def test_generate_legend_in_layout(
+    nc_layer: QgsVectorLayer,
+    qgs_layout: QgsLayout,
+    qgs_project: QgsProject,
+    layout_page_a4: QgsLayoutItemPage,
+    prepare_bivariate_renderer,
+    layout_space,
+    export_page_to_image,
+):
+
+    bivariate_renderer = prepare_bivariate_renderer(
+        nc_layer, field1="PERIMETER", field2="AREA", color_ramp=BivariateColorRampGreenPink()
+    )
+
+    nc_layer.setRenderer(bivariate_renderer)
+
+    qgs_project.addMapLayer(nc_layer)
+
+    layout_item_map = QgsLayoutItemMap(qgs_layout)
+    layout_item_map.setLayers([nc_layer])
+    layout_item_map.attemptSetSceneRect(layout_space)
+    layout_item_map.setCrs(nc_layer.crs())
+    layout_item_map.zoomToExtent(nc_layer.extent())
+
+    qgs_layout.addItem(layout_item_map)
+
+    file = "./tests/images/image.png"
+
+    export_page_to_image(qgs_layout, layout_page_a4, file)
+
+    assert_images_equal(file, "./tests/images/correct/layout_item_legend.png")
