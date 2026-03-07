e8330212578f6533b6add1075904b584181d70e2
Jan Caha <jan.caha@outlook.com>
2026-03-07 10:31:41 +0100

    add test cases for manual ramps

--- tests/test_color_ramps.py
+++ tests/test_color_ramps.py
@@ -1,6 +1,6 @@
 import pytest
-from qgis.core import QgsGradientColorRamp, QgsReadWriteContext
-from qgis.PyQt.QtGui import QIcon
+from qgis.core import QgsGradientColorRamp
+from qgis.PyQt.QtGui import QColor, QIcon
 from qgis.PyQt.QtXml import QDomDocument
 
 from BivariateRenderer.colormixing.color_mixing_method import ColorMixingMethodDarken
@@ -13,6 +13,7 @@ from BivariateRenderer.colorramps.bivariate_color_ramp import (
     BivariateColorRampGreenPink,
     BivariateColorRampGreenPurple,
     BivariateColorRampLigthYellowPurple,
+    BivariateColorRampManual,
     BivariateColorRampOrangeBlue,
     BivariateColorRampOrangePurple,
     BivariateColorRampPinkBlue,
@@ -105,3 +106,47 @@ def test_gradient_ramp_clone():
     # cloned is independent
     cloned_ramp.set_number_of_classes(2)
     assert ramp.number_of_classes == 4
+
+
+def test_manual_ramp_save_load():
+    colors = [
+        [QColor("#ff0000"), QColor("#00ff00"), QColor("#0000ff")],
+        [QColor("#ffff00"), QColor("#ff00ff"), QColor("#00ffff")],
+        [QColor("#ffffff"), QColor("#808080"), QColor("#000000")],
+    ]
+    ramp = BivariateColorRampManual(colors)
+
+    doc = QDomDocument("test")
+    elem = ramp.save(doc)
+
+    loaded = BivariateColorRampManual.load(elem)
+
+    assert loaded.number_of_classes == ramp.number_of_classes
+    for i in range(ramp.number_of_classes):
+        for j in range(ramp.number_of_classes):
+            assert loaded.get_color(i, j).name() == ramp.get_color(i, j).name()
+
+
+def test_manual_ramp_clone_independence():
+    colors = [
+        [QColor("#ff0000"), QColor("#00ff00")],
+        [QColor("#0000ff"), QColor("#ffff00")],
+    ]
+    ramp = BivariateColorRampManual(colors)
+    cloned = ramp.clone()
+
+    for i in range(ramp.number_of_classes):
+        for j in range(ramp.number_of_classes):
+            assert cloned.get_color(i, j).name() == ramp.get_color(i, j).name()
+
+    # cloned has its own separate color lists
+    assert cloned._colors is not ramp._colors
+    assert cloned._colors[0] is not ramp._colors[0]
+
+
+def test_manual_ramp_requires_square_colors():
+    with pytest.raises(ValueError, match="square"):
+        BivariateColorRampManual([
+            [QColor("#ff0000"), QColor("#00ff00")],
+            [QColor("#0000ff")],
+        ])
